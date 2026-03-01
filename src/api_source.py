637e03fd471a07e13300246ed5b4a6ffdf885bc6
AlRoman747 <romaaltahir@icloud.com>
2026-03-01 09:40:06 +0300

    fixed random, add generated by seed

--- src/api_source.py
+++ src/api_source.py
@@ -4,18 +4,28 @@ from random import randint
 
 class APISimulate:
     def __init__(self):
+        self.tasks = []
         self.priority_types = ['low', 'medium', 'high']
         self.data = [
-            {"task_id": randint(0, 1000)} for i in range(10)
+            {"task_id": 748},
+            {"task_id": 374},
+            {"task_id": 187},
+            {"task_id": 562},
+            {"task_id": 281},
+            {"task_id": 844},
+            {"task_id": 422},
+            {"task_id": 211},
+            {"task_id": 634},
+            {"task_id": 317},
         ]
         for i in range(10):
             self.data[i].setdefault("task", f"task #{self.data[i]['task_id']}")
             self.data[i].setdefault("priority", self.priority_types[randint(0,2)])
 
+
     def get_tasks(self) -> list[Task]:
-        tasks = []
         for item in self.data:
-            tasks.append({
+            self.tasks.append({
                 "id": item["task_id"],
                 "payload": {
                     "text": item["task"],
@@ -23,4 +33,4 @@ class APISimulate:
                     "source": "api"
                 }
             })
-        return tasks
+        return self.tasks
--- src/random_source.py
+++ src/random_source.py
@@ -3,15 +3,21 @@ from random import randint
 
 
 class RandomTaskGenerate:
+    def __init__(self, seed=1):
+        self.seed = seed
+        self.tasks = {}
     def get_tasks(self) -> list[Task]:
-        tasks = []
 
-        for i in range(randint(1, 50)):
-            tasks.append(
-                {
-                    "id": i,
-                    "payload": f"task #{i}"
-                }
-            )
+        self.tasks.setdefault(self.seed, [])
+        if self.tasks[self.seed]:
+            return self.tasks[self.seed]
+        else:
+            for i in range(randint(1, 50)):
+                self.tasks[self.seed].append(
+                    {
+                        "id": i,
+                        "payload": f"task #{i}"
+                    }
+                )
 
-        return tasks
+        return self.tasks[self.seed]
