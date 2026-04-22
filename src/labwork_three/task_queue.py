11a17f26b0db293733cf3b9fce44f2665aeb5701
AlRoman747 <romaaltahir@icloud.com>
2026-04-22 15:47:36 +0300

    fixed task  queue struct

--- src/labwork_three/task_queue.py
+++ src/labwork_three/task_queue.py
@@ -1,6 +1,4 @@
-import copy
-from typing import Iterable
-import queue
+from collections import deque
 from typing import Any, Iterator
 
 from src.labwork_two.find_source_tasks import Task
@@ -8,35 +6,28 @@ from src.labwork_two.find_source_tasks import Task
 
 class TaskQueue:
     def __init__(self):
-        """ create an empty queue from collections framework"""
-        self._tasks = queue.Queue()
-
+        self._tasks = deque()
 
     def add(self, task: Task) -> None:
         """adding tasks in queue"""
-        self._tasks.put(task)
+        self._tasks.append(task)   # FIFO: добавляем вправо
 
-    def pop(self) -> None:
+    def pop(self):
         """removing tasks from queue"""
         if not self._tasks:
             raise IndexError("Queue is empty")
-        return self._tasks.get()
+        return self._tasks.popleft()  # забираем слева
 
     def empty(self):
-        return self._tasks.empty()
+        return len(self._tasks) == 0
 
     def __len__(self):
-        return self._tasks.qsize()
+        return len(self._tasks)
 
-    def __iter__(self):
-        return self
-    def __next__(self):
-        task = self._tasks.get()
-        self.
+    def __iter__(self) -> Iterator:
+        return iter(self._tasks)
 
-
-    def lazy_filter(self, param: Any, status: str) -> Iterator[Task]:
-        """filter for any parameter from class Task from labwork two"""
+    def lazy_filter(self, param: Any, status: str) -> Iterator:
         for task in self._tasks:
             if getattr(task, param).value == status:
                 yield task
